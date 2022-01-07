ac30f2281c14cb21d13d331d09352bd6e94e8eb0
Toby Yang <tianer2820@163.com>
2022-01-07 18:41:02 -0500

    implement FK to IK

--- fk_ik_switch/fk_ik_switch.py
+++ fk_ik_switch/fk_ik_switch.py
@@ -27,6 +27,7 @@ bl_info = {
 utility functions
 """
 
+
 def follow_bone_chain(bone: bpy.types.PoseBone, count: int) -> List[bpy.types.PoseBone]:
     chain = []
     current = bone
@@ -55,9 +56,25 @@ def selected_bones(object: bpy.types.Object) -> List[bpy.types.PoseBone]:
     return bones
 
 
+class IKChain:
+    def __init__(self, bones: List[bpy.types.PoseBone], constraint: bpy.types.KinematicConstraint) -> None:
+        self.bones = bones
+        self.constraint = constraint
+        self.target = constraint.target
+        self.subtarget = None
+        if self.target is not None and self.target.type == 'ARMATURE':
+            self.subtarget = self.target.pose.bones[self.constraint.subtarget]
+
+        self.pole = constraint.pole_target
+        self.subpole = None
+        if self.pole is not None and self.pole.type == 'ARMATURE':
+            self.subpole = self.pole.pose.bones[self.constraint.pole_subtarget]
+
+
 """
 Operators
 """
+
 
 class ToggleFKIK(bpy.types.Operator):
     """Switch IK on/off"""
@@ -77,8 +94,8 @@ class ToggleFKIK(bpy.types.Operator):
         default="TOGGLE")
 
     insert_keyframe: bpy.props.BoolProperty(name="Insert Keyframe",
-        description="Insert Keyframe for bones and IK constraints",
-        default=True)
+                                            description="Insert Keyframe for bones and IK constraints",
+                                            default=True)
 
     @classmethod
     def poll(cls, context: bpy.types.Context) -> bool:
@@ -93,30 +110,40 @@ class ToggleFKIK(bpy.types.Operator):
             self.report({'ERROR'}, 'Toggle is unimplemented yet')
             return {'CANCELLED'}
         elif self.action == 'FK2IK':
-            self.report({'ERROR'}, 'FK2IK is unimplemented yet')
-            return {'CANCELLED'}
+            return self.fk2ik(context)
         elif self.action == 'IK2FK':
             return self.ik2fk(context)
         else:
-            self.report({'ERROR'}, "Unknown action type: {}".format(self.action))
+            self.report(
+                {'ERROR'}, "Unknown action type: {}".format(self.action))
             return {'CANCELLED'}
-    
-    def ik2fk(self, context: bpy.types.Context) -> Set[str]:
+
+    def try_get_obj_bones(self, context: bpy.types.Context) -> List:
+        """
+        return (active_object, selected pose bones) if valid,
+        otherwise report an error and return None
+        """
         obj = context.view_layer.objects.active
         if obj is None:
             self.report({'OPERATOR'}, 'No object selected')
-            return {"CANCELLED"}
+            return None
         pose = obj.pose
         if pose is None:
             self.report({'OPERATOR'}, 'Active object does not have pose data')
-            return {"CANCELLED"}
+            return None
         pose: bpy.types.Pose
 
         bones = selected_bones(obj)
         if bones is None:
             self.report({'OPERATOR'}, 'No bone selected')
-            return {"CANCELLED"}
-        bones = bones.copy()
+            return None
+        return obj, bones
+
+    def ik2fk(self, context: bpy.types.Context) -> Set[str]:
+        obj_bones = self.try_get_obj_bones(context)
+        if obj_bones is None:
+            return {'CANCELLED'}
+        obj, bones = obj_bones
 
         ik_bones: List[bpy.types.PoseBone] = []
         ik_constraints = []
@@ -132,34 +159,143 @@ class ToggleFKIK(bpy.types.Operator):
                     chain = follow_bone_chain(bone, chain_length)
                     ik_bones.extend(chain)
                     ik_constraints.append(constraint)
-        
-        # apply pose and select affected bones
-        bpy.ops.pose.select_all(action='DESELECT')
+
+        # apply pose
         for bone in ik_bones:
             bone: bpy.types.PoseBone
-            converted = obj.convert_space(pose_bone=bone, matrix=bone.matrix, from_space='POSE', to_space='LOCAL')
-            bone.bone.select = True
+            converted = obj.convert_space(
+                pose_bone=bone, matrix=bone.matrix, from_space='POSE', to_space='LOCAL')
             current_frame = context.scene.frame_current
             if self.insert_keyframe:
-                bone.keyframe_insert('rotation_quaternion', frame=current_frame - 1, group='FKIK Pose')
+                bone.keyframe_insert('rotation_quaternion', frame=current_frame - 1)
             bone.matrix_basis = converted
             if self.insert_keyframe:
-                bone.keyframe_insert('rotation_quaternion', frame=current_frame, group='FKIK Pose')
-        
-        # change constrint weight
+                bone.keyframe_insert('rotation_quaternion', frame=current_frame)
+
+        # change constrint influence
         for constraint in ik_constraints:
             current_frame = context.scene.frame_current
             if self.insert_keyframe:
-                constraint.keyframe_insert('influence', frame=current_frame - 1, group='IK Weight')
+                constraint.keyframe_insert(
+                    'influence', frame=current_frame - 1, group='IK Weight')
             constraint.influence = 0
             if self.insert_keyframe:
-                constraint.keyframe_insert('influence', frame=current_frame, group='IK Weight')
-        
+                constraint.keyframe_insert(
+                    'influence', frame=current_frame, group='IK Weight')
+
         return {'FINISHED'}
+
+    def fk2ik(self, context: bpy.types.Context) -> Set[str]:
+        # get required context
+        obj_bones = self.try_get_obj_bones(context)
+        if obj_bones is None:
+            return {'CANCELLED'}
+        obj, bones = obj_bones
+        obj: bpy.types.Object
+        bones: List[bpy.types.PoseBone]
+        current_frame = context.scene.frame_current
+
+        # detect IK chains
+        ik_chains: List[IKChain] = []
+        for bone in bones:
+            # find IK head
+            constraints = bone.constraints
+            for constraint in constraints:
+                constraint: bpy.types.Constraint
+                if constraint.type == 'IK':
+                    # is an ik head
+                    constraint: bpy.types.KinematicConstraint
+                    chain_length = constraint.chain_count
+                    chain_bones = follow_bone_chain(bone, chain_length)
+
+                    ik_chain = IKChain(chain_bones, constraint)
+                    ik_chains.append(ik_chain)
+
+        # convert each IK chain
+        for chain in ik_chains:
+            # calc the pole vector
+            root = chain.bones[-1]
+            leaf = chain.bones[0]
+            chain_vector: Vector = leaf.tail - root.head
+            chain_pole: Vector = Vector()
+            for bone in chain.bones:
+                relative: Vector = bone.center - root.head
+                projected: Vector = relative.project(chain_vector)
+                pole: Vector = relative - projected
+                chain_pole += pole
+            chain_pole /= len(chain.bones)
+            chain_pole.magnitude = max(chain_pole.length * 4, chain_vector.length / 2)
+            pole_location = root.head + chain_vector / 2 + chain_pole
+
+            # set pole object location
+            if chain.pole is not None:
+                world_mat: Matrix = obj.convert_space(pose_bone=root, matrix=Matrix.Translation(pole_location),
+                                                      from_space='POSE', to_space='WORLD')
+                if chain.subpole is None:
+                    # for object target
+                    converted = chain.pole.convert_space(matrix=world_mat,
+                                                         from_space='WORLD', to_space='LOCAL')
+                    if self.insert_keyframe:
+                        chain.target.keyframe_insert('location', frame=current_frame - 1)
+                    chain.pole.location = converted.translation
+                    if self.insert_keyframe:
+                        chain.target.keyframe_insert('location', frame=current_frame)
+                else:
+                    # for bone target
+                    converted = chain.pole.convert_space(pose_bone=chain.subpole,
+                                                         matrix=world_mat,
+                                                         from_space='WORLD', to_space='LOCAL')
+                    if self.insert_keyframe:
+                        chain.subtarget.keyframe_insert('location', frame=current_frame - 1)
+                    chain.subpole.location = converted.translation
+                    if self.insert_keyframe:
+                        chain.subtarget.keyframe_insert('location', frame=current_frame)
+            
+            # set target object location
+            if chain.target is not None:
+                world_mat: Matrix = obj.convert_space(pose_bone=root, matrix=Matrix.Translation(leaf.tail),
+                                                      from_space='POSE', to_space='WORLD')
+                if chain.subtarget is None:
+                    # for object target
+                    converted = chain.target.convert_space(matrix=world_mat,
+                                                         from_space='WORLD', to_space='LOCAL')
+                    if self.insert_keyframe:
+                        chain.target.keyframe_insert('location', frame=current_frame - 1)
+                    chain.target.location = converted.translation
+                    if self.insert_keyframe:
+                        chain.target.keyframe_insert('location', frame=current_frame)
+                else:
+                    # for bone target
+                    converted = chain.target.convert_space(pose_bone=chain.subtarget,
+                                                         matrix=world_mat,
+                                                         from_space='WORLD', to_space='LOCAL')
+                    if self.insert_keyframe:
+                        chain.subtarget.keyframe_insert('location', frame=current_frame - 1)
+                    chain.subtarget.location = converted.translation
+                    if self.insert_keyframe:
+                        chain.subtarget.keyframe_insert('location', frame=current_frame)
+            # change constraint influence
+            if self.insert_keyframe:
+                chain.constraint.keyframe_insert('influence', frame=current_frame - 1, group='IK Weight')
+            chain.constraint.influence = 1.0
+            if self.insert_keyframe:
+                chain.constraint.keyframe_insert('influence', frame=current_frame, group='IK Weight')
+            # clear bone pose
+            for bone in chain.bones:
+                if self.insert_keyframe:
+                    bone.keyframe_insert('rotation_quaternion', frame=current_frame - 1)
+                bone.rotation_quaternion = Quaternion()
+                if self.insert_keyframe:
+                    bone.keyframe_insert('rotation_quaternion', frame=current_frame)
+
+        return {'FINISHED'}
+
 
 """
 UIs
 """
+
+
 class VIEW3D_PT_animation_fkik_switch(bpy.types.Panel):
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
@@ -183,10 +319,13 @@ class VIEW3D_PT_animation_fkik_switch(bpy.types.Panel):
 """
 Register/Unregister functions
 """
+
+
 def register():
     bpy.utils.register_class(ToggleFKIK)
     bpy.utils.register_class(VIEW3D_PT_animation_fkik_switch)
     # bpy.types.VIEW3D_MT_object.append(menu_func)
+
 
 def unregister():
     bpy.utils.unregister_class(ToggleFKIK)
